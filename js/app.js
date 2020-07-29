a1c48a42e2d8065564bb5c861d3b3c7f51168f06
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-07-29 01:17:56 -0400

    moved text to data object and binded data for seperation of concern

--- js/app.js
+++ js/app.js
@@ -11,6 +11,14 @@ new Vue({
       extraLarge: 1200
     },
     currentWidth: null,
+    title: `Lorem ipsum dolor sit amet, consectetur adipiscing elit`,
+    subtitle: `Lorem ipsum dolor sit amet consctur adipiuing elit onec pulneque ut rstibr`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque congue mauris rhoncus augue varius congue. Phasellus lacinia eros vitae nunc ornare, sit amet auctor ante pharetra. Nunc lorem odio, iaculis sit amet dapibus varius, efficitur nec ligula. Etiam ac felis lacinia, euismod velit at, imperdiet massa. Donec ornare est quis finibus cursus. Donec ac leo purus.`,
+    buttonText: `DONEC ULLMCORPER QUA`,
+    img: {
+      src: `img/flowers.jpeg`,
+      alt: `pink dahlias at sunset bokeh`,
+    },
   },
   created() {
     window.addEventListener("resize", this.calcCurrentWidth);
